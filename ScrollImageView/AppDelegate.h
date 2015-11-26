6294150713b6cde87ed1675063644ebabb1e314f
floiges <floiges@163.com>
2015-11-26 19:59:20 +0800

    first commit

---
+++ ScrollImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollImageView
+//
+//  Created by 224 on 15/11/26.
+//  Copyright (c) 2015年 zoomlgd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollImageView
+//
+//  Created by 224 on 15/11/26.
+//  Copyright (c) 2015年 zoomlgd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
